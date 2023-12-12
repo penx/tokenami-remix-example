16927737e1466a0ff855dba04750f884d3bc83a9
Alasdair McLeay <alasdair.mcleay@me.com>
2023-12-12 09:44:00 +0000

    npx tokenami init

---
+++ .tokenami/tokenami.config.js
@@ -0,0 +1,25 @@
+const { createConfig } = require('@tokenami/dev');
+
+module.exports = createConfig({
+  include: [],
+  grid: '0.25rem',
+  responsive: {},
+  theme: {
+    alpha: {},
+    anim: {},
+    border: {},
+    color: {},
+    ease: {},
+    'font-size': {},
+    leading: {},
+    'line-style': {},
+    radii: {},
+    size: {},
+    shadow: {},
+    surface: {},
+    tracking: {},
+    transition: {},
+    weight: {},
+    z: {},
+  },
+});
